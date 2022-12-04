2bac1b29ec2e60500c7688972420f32a1e171806
Rodrigo Alonso <rodrigo.alonso@urbetrack.com>
2022-12-04 16:58:49 -0300

    Rutas privadas

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -2,13 +2,20 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom';
 import { LoginPage } from '../Auth';
 import { HeroRoutes } from '../Heroes';
+import { PrivateRoutes } from './PrivateRoutes';
 
 export const AppRouter = () => {
     return (
         <>
             <Routes>
                 <Route path='login' element={ <LoginPage /> } />
-                <Route path='/*' element={ <HeroRoutes /> } />
+
+                <Route path='/*' element={ 
+                    <PrivateRoutes>
+                        <HeroRoutes />
+                    </PrivateRoutes>
+                 } />
+
             </Routes>
         </>
     )

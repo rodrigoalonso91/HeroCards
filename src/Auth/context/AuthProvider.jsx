8b4890c4deeee8995126a0389c993476d2a6b8c9
Rodrigo Alonso <rodrigo.alonso@urbetrack.com>
2022-12-04 16:36:13 -0300

    mantener usuario activo

--- src/Auth/context/AuthProvider.jsx
+++ src/Auth/context/AuthProvider.jsx
@@ -1,22 +1,35 @@
 import { useReducer } from "react"
 import { AuthContext } from "./AuthContext"
 import { authReducer } from "./authReducer";
-import { types  } from "../types/types";
+import { types } from "../types/types";
+
+const init = () => {
+
+    const user = JSON.parse( localStorage.getItem('user') );
+
+    return {
+        logged: !!user,
+        user: user
+    }
+}
 
 export const AuthProvider = ({ children }) => {
 
-    const [ authState, dispath ] = useReducer( authReducer, {logged: false} );
+    const [ authState, dispath ] = useReducer( authReducer, {}, init );
 
     const login = ( name = '' ) => {
 
-        const action = {
-            type: types.login,
-            payload: {
-                id: 'ABC',
-                name
-            }
+        const user = {
+            id: 'ABC',
+            name
         }
 
+        const action = {
+            type: types.login,
+            payload: user
+        }
+
+        localStorage.setItem( 'user', JSON.stringify(user) )
         dispath(action);
     };
 
